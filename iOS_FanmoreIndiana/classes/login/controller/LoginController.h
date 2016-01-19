bc2b324f68d264827814e3e1e1f8d390350a7a85
stricker <liuchen195@163.com>
2016-01-19 15:22:31 +0800

    312321312

---
+++ iOS_FanmoreIndiana/classes/login/controller/LoginController.h
@@ -0,0 +1,13 @@
+//
+//  LoginController.h
+//  iOS_FanmoreIndiana
+//
+//  Created by 刘琛 on 16/1/19.
+//  Copyright © 2016年 刘琛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginController : UIViewController
+
+@end
