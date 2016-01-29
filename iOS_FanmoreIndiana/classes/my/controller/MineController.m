b54d9602ef8df0a5100a348cdb104e03856c2399
stricker <liuchen195@163.com>
2016-01-29 14:02:10 +0800

    mine 页面修改

--- iOS_FanmoreIndiana/classes/my/controller/MineController.m
+++ iOS_FanmoreIndiana/classes/my/controller/MineController.m
@@ -98,6 +98,7 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
