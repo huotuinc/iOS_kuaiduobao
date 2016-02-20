30e6844eb76e19242d536fb9f8c05bfbf9005705
stricker <liuchen195@163.com>
2016-02-20 17:44:41 +0800

    地址修改

--- iOS_FanmoreIndiana/classes/my/controller/AddAddressController.m
+++ iOS_FanmoreIndiana/classes/my/controller/AddAddressController.m
@@ -20,16 +20,41 @@
     [SVProgressHUD setDefaultMaskType:SVProgressHUDMaskTypeBlack];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] bk_initWithTitle:@"保存" style:UIBarButtonItemStylePlain handler:^(id sender) {
         
+        [self addNewAddress];
     }];
 
 }
 
 - (void)addNewAddress {
-    if (_personName.text != nil) {
+    if (_personName.text == nil) {
         [SVProgressHUD showErrorWithStatus:@"请输入用户名"];
-    }else if (_personIphone.text != nil) {
+    }else if (_personIphone.text == nil) {
         [SVProgressHUD showErrorWithStatus:@"请输入手机号"];
+    }else if (_detailAddress.text == nil) {
+        [SVProgressHUD showErrorWithStatus:@"请输入详细地址"];
+    }else {
+        if ([self checkTel:_personIphone.text]) {
+            
+            [self postAddressToServer];
+            
+        }else {
+            [SVProgressHUD showErrorWithStatus:@"请输入正确的手机号"];
+        }
     }
+}
+
+- (void)postAddressToServer {
+    NSMutableDictionary *dic = [NSMutableDictionary dictionary];
+    dic[@"receiver"] = self.personName.text;
+    dic[@"mobile"] = self.personIphone.text;
+    dic[@"details"] = self.detailAddress.text;
+    dic[@"defaultAddress"] = @(self.defaultAddress.on);
+    
+    [UserLoginTool loginRequestPostWithFile:@"addMyAddress" parame:dic success:^(id json) {
+        LWLog(@"%@",json);
+    } failure:^(NSError *error) {
+        LWLog(@"%@",error);
+    } withFileKey:nil];
 }
 
 
---
+++ iOS_FanmoreIndiana/classes/my/model/AddressModel.m
@@ -0,0 +1,15 @@
+//
+//  AddressModel.m
+//  iOS_FanmoreIndiana
+//
+//  Created by 刘琛 on 16/2/20.
+//  Copyright © 2016年 刘琛. All rights reserved.
+//
+
+#import "AddressModel.h"
+
+@implementation AddressModel
+
+
+
+@end
