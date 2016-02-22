045d50264bacec171cbdeca0f858a3d86d3d20d2
stricker <liuchen195@163.com>
2016-02-22 15:15:56 +0800

    1232131321

---
+++ iOS_FanmoreIndiana/classes/my/controller/MineShareController.h
@@ -0,0 +1,15 @@
+//
+//  MineShareController.h
+//  iOS_FanmoreIndiana
+//
+//  Created by 刘琛 on 16/2/22.
+//  Copyright © 2016年 刘琛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MineShareController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
---
+++ iOS_FanmoreIndiana/classes/my/controller/MineShareController.m
@@ -0,0 +1,47 @@
+//
+//  MineShareController.m
+//  iOS_FanmoreIndiana
+//
+//  Created by 刘琛 on 16/2/22.
+//  Copyright © 2016年 刘琛. All rights reserved.
+//
+
+#import "MineShareController.h"
+#import "DetailShareTableViewCell.h"
+
+@interface MineShareController ()<UITableViewDelegate,UITableViewDataSource>
+
+@end
+
+@implementation MineShareController
+
+static NSString *shareCIdentifier = @"shareCIdentifier";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self.tableView registerNib:[UINib nibWithNibName:@"DetailShareTableViewCell" bundle:nil] forCellReuseIdentifier:shareCIdentifier];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark table
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return ADAPT_HEIGHT(500);
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    DetailShareTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:shareCIdentifier forIndexPath:indexPath];
+    return cell;
+}
+
+@end
