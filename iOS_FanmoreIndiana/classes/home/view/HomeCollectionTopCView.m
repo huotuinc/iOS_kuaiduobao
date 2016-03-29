d11f7c36a093352a7425bf129c53a1a60e6d6740
che <BigChe777@163.com>
2016-03-29 10:16:50 +0800

    debug homeCollectionV

--- iOS_FanmoreIndiana/classes/home/view/HomeCollectionTopCView.m
+++ iOS_FanmoreIndiana/classes/home/view/HomeCollectionTopCView.m
@@ -12,6 +12,21 @@
 
 - (void)awakeFromNib {
     // Initialization code
+    
+    NSArray *arrTitle=@[@"分类",@"专区商品",@"红包专区",@"晒单",@"常见问题"];
+    for (int i=0 ; i<5 ; i++) {
+        UILabel *label=[self viewWithTag:100+i];
+        label.text=arrTitle[i];
+        [UILabel changeLabel:label AndFont:24 AndColor:COLOR_TEXT_TITILE];
+    }
+    NSArray *arrImage=@[@"home_content_fenlei",@"home_content_zhuanqu",@"home_content_hongbao",@"home_content_shaidan",@"home_content_wenti"];
+    for (int i=0; i<5; i++) {
+        UIImageView *imageV=[self viewWithTag:200+i];
+        imageV.image=[UIImage imageNamed:[NSString stringWithFormat:@"%@",arrImage[i]]];
+    }
+    
+    
+    
 }
 
 @end
