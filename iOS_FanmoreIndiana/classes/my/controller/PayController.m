816741a8b1a60c73702aef7d0dc15de521fadc04
che <BigChe777@163.com>
2016-04-21 09:37:45 +0800

    debug ALIPay error

--- iOS_FanmoreIndiana/classes/my/controller/PayController.m
+++ iOS_FanmoreIndiana/classes/my/controller/PayController.m
@@ -314,7 +314,7 @@ static NSString *payIdentify = @"payIdentifty";
     order.showUrl = @"m.alipay.com";
     
     //应用注册scheme,在AlixPayDemo-Info.plist定义URL types
-    NSString *appScheme = @"newfanmore2015";
+    NSString *appScheme = @"qibinTreasure2016";
     
     //将商品信息拼接成字符串
     NSString *orderSpec = [order description];
