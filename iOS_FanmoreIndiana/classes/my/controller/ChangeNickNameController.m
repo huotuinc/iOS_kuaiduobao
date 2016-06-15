55fc6f864200f6c1ce42f54b2388bd7b04f44ef7
stricker <liuchen195@163.com>
2016-06-15 14:26:36 +0800

    中文输入限制

--- iOS_FanmoreIndiana/classes/my/controller/ChangeNickNameController.m
+++ iOS_FanmoreIndiana/classes/my/controller/ChangeNickNameController.m
@@ -17,6 +17,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    self.textField.autocorrectionType=UITextAutocorrectionTypeNo;
+    [self.textField addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
     
 }
 
@@ -60,11 +62,21 @@
     
 }
 
-- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
-    if (textField.text.length + string.length > 11) {
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    NSString * toBeString = [textField.text stringByReplacingCharactersInRange:range withString:string];
+    
+    if (toBeString.length > 11 ) {
         return NO;
     }
     return YES;
+}
+
+- (void)textFieldDidChange:(UITextField *)textField
+{
+    if (textField.text.length > 11) {
+        textField.text = [textField.text substringToIndex:11];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -86,6 +98,40 @@
     
 }
 
+
+//- (void)textFieldDidChange:(UITextField *)textField {
+//    NSString *toBeString = textField.text;
+//    NSArray *currentar = [UITextInputMode activeInputModes];
+//    UITextInputMode *current = [currentar firstObject];
+//    
+//    //下面的方法是iOS7被废弃的，注释
+//    //    NSString *lang = [[UITextInputMode currentInputMode] PRimaryLanguage]; // 键盘输入模式
+//    
+//    if ([current.primaryLanguage isEqualToString:@"zh-Hans"]) { // 简体中文输入，包括简体拼音，健体五笔，简体手写
+//        UITextRange *selectedRange = [textField markedTextRange];
+//        //获取高亮部分
+//        UITextPosition *position = [textField positionFromPosition:selectedRange.start offset:0];
+//        // 没有高亮选择的字，则对已输入的文字进行字数统计和限制
+//        if (!position) {
+//            if (toBeString.length > 14) {
+//                textField.text = [toBeString substringToIndex:14];
+//            }
+//        }
+//        // 有高亮选择的字符串，则暂不对文字进行统计和限制
+//        else{
+//            if (toBeString.length > 14) {
+//                textField.text = [toBeString substringToIndex:14];
+//            }
+//        }
+//    }
+//    // 中文输入法以外的直接对其统计限制即可，不考虑其他语种情况
+//    else{
+//        if (toBeString.length > 14) {
+//            textField.text = [toBeString substringToIndex:14];
+//        }
+//    }
+//    NSLog(@"%@",textField.text);
+//}
 
 /*
 #pragma mark - Navigation
